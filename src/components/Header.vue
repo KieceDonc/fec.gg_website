f3c24b0bd2ac182a00f5ce596bbf9c635b1943df
KieceDonc <valentin.verstracte@gmail.com>
2022-05-28 17:45:55 +0200

    update brand / header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,6 +1,6 @@
 <template>
-	<header class="headerNormal">
-		<div id="headerContainer">
+	<div id="headerContainer" class="headerNormal">
+		<div id="brandContainer">
 			<div id="brand">
 				Fec
 				<div id="brandPoint">.</div>
@@ -25,7 +25,7 @@
 			<a>SSBU</a>
 			<a>Others</a>
 		</div>
-	</header>
+	</div>
 </template>
 
 <script>
@@ -33,22 +33,22 @@ export default {
 	name: 'Header',
 };
 
-window.addEventListener('scroll', function (e) {
+window.addEventListener('scroll', function () {
 	let pageYOffset = window.pageYOffset;
 	updateHeader(pageYOffset);
 	updateCategory(pageYOffset);
 });
 
 function updateHeader(pageYOffset) {
-	let headerElement = document.getElementById('headerContainer').parentElement;
+	let brandElement = document.getElementById('brandContainer').parentElement;
 	let registerElement = document.getElementById('register');
 	let brandPointElement = document.getElementById('brandPoint');
 	if (pageYOffset == 0) {
-		headerElement.className = 'headerNormal';
+		brandElement.className = 'brandContainer headerNormal';
 		registerElement.className = 'headerButton registerNormal';
 		brandPointElement.className = 'brandPointNormal';
 	} else {
-		headerElement.className = 'headerAfterScroll';
+		brandElement.className = 'brandContainer headerAfterScroll';
 		registerElement.className = 'headerButton registerAfterScroll';
 		brandPointElement.className = 'brandPointAfterScroll';
 	}
@@ -67,7 +67,7 @@ function updateCategory(pageYOffset) {
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-header {
+#headerContainer {
 	padding-top: 20px;
 	padding-bottom: 20px;
 	position: fixed;
@@ -86,8 +86,91 @@ header {
 	transition: background-color 400ms linear;
 }
 
-#headerContainer {
+#brandContainer {
 	display: flex;
 	justify-content: space-between;
+}
+
+#brand {
+	display: flex;
+	font-weight: bold;
+	font-size: 30px;
+	margin-left: 100px;
+}
+
+#brandPoint {
+	font-size: 58px;
+	line-height: 0.3;
+}
+
+.brandPointNormal {
+	color: white;
+}
+
+.brandPointAfterScroll {
+	color: #325aa5;
+}
+
+#headerMenu {
+	margin-top: auto;
+	margin-bottom: auto;
+}
+
+.headerButton {
+	padding-left: 15px;
+	padding-right: 15px;
+	padding-top: 5px;
+	padding-bottom: 5px;
+	font-size: 18px;
+}
+
+#register {
+	border-radius: 4px;
+	margin-right: 100px;
+}
+
+.registerNormal {
+	border: 2px solid white;
+}
+
+.registerAfterScroll {
+	border: 2px solid rgb(50, 90, 165);
+}
+
+#categoryContainer {
+	display: flex;
+	justify-content: space-evenly;
+	transition: all 0.4s ease-in-out;
+	color: #555555;
+}
+
+.categoryContainerHidden {
+	opacity: 0;
+	height: 0;
+	overflow: hidden;
+	margin-top: 0px;
+	border: 0;
+}
+
+.categoryContainerAfterScroll {
+	opacity: 1;
+	height: auto;
+	margin-top: 20px;
+	border-top: 1px solid #cccccc;
+	border-bottom: 1px solid #cccccc;
+}
+
+#categoryContainer > a {
+	padding-top: 20px;
+	padding-bottom: 20px;
+	padding-left: auto;
+	padding-right: auto;
+	display: table-cell;
+	width: 100%;
+	text-align: center;
+}
+
+#categoryContainer > a:hover {
+	text-decoration: underline;
 }
 </style>

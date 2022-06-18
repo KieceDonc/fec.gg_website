7641ce46132dec80317be98beda0260c329a8e74
KieceDonc <valentin.verstracte@gmail.com>
2022-06-18 14:41:15 +0200

    Update login + introduce of google sign in button

---
+++ src/views/Auth/Login.vue
@@ -0,0 +1,12 @@
+<template>
+  <h1>Content de te revoir !</h1>
+  <p>Vous n'avez pas encore de compte ? Inscrivez-vous ici</p>
+  <div>
+    <button type="button" class="login-with-google-btn">
+      Sign in with Google
+    </button>
+    <p>Continuer avec l'addresse mail</p>
+  </div>
+</template>
+
+<style scoped></style>
